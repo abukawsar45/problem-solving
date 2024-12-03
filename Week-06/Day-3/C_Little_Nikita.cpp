10621bb6d5917a3427e9a49a5197f0be363a4bb4
Abu Kawsar <121648061+abukawsar45@users.noreply.github.com>
2024-12-03 20:25:26 +0600

    Add files via upload
    week6 day3

---
+++ Week-06/Day-3/C_Little_Nikita.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  int t;
+  cin >> t;
+  while (t--) 
+  {
+    int n,m;
+    cin >> n>>m;
+    if(n>=m){
+      if ((n % 2 == 0 && m % 2 == 0) || (n % 2 != 0 && m % 2 != 0))
+        cout << "Yes" << endl;
+      else cout << "No" << endl;
+    }
+    else cout<<"No"<<endl;
+    
+    
+  }
+  return 0;
+}
