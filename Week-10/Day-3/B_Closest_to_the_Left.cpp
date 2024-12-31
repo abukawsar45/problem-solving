350b1358c5fefdd4900486630d71603daca50aab
Abu Kawsar <121648061+abukawsar45@users.noreply.github.com>
2024-12-31 21:43:27 +0600

    Add files via upload
    week10 day3

---
+++ Week-10/Day-3/B_Closest_to_the_Left.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define pr pair<int, int>
+#define prl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define cyes cout << "Yes" << endl
+#define cno cout << "No" << endl
+#define minus cout << -1 << endl
+#define fastread()              \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+using namespace std;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  for (int &i : a)
+    cin >> i;
+
+  for (int i = 0; i < q; i++)
+  {
+    int key, l = 0, r = n - 1, mid;
+    cin >> key;
+    int ans = 0;
+    while (l <= r)
+    {
+      mid = (l + r) / 2;
+      if (key >= a[mid]  )
+      {
+        ans = mid+1;
+        l= mid + 1;
+      }
+      
+      else
+        r = mid - 1;
+    }
+    cout << ans << endl;
+  }
+}
+
+int32_t main()
+{
+  fastread();
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
