40915af85b339d67ab673c8b4efb3a13fc417ab7
Abu Kawsar <121648061+abukawsar45@users.noreply.github.com>
2025-01-17 23:54:16 +0600

    Add files via upload
    week12 day5

---
+++ Week-12/Day-5/C_T_primes.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define pr pair<int, int>
+#define prl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define cyes cout << "Yes" << endl
+#define cno cout << "No" << endl
+#define minus cout << -1 << endl
+#define fastread()              \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+using namespace std;
+const int mod = 1e9 + 7;
+
+bool isPrime(int n)
+{
+  if(n == 1) return false;
+
+  for (int i = 2; i * i <= n; i++)
+  {
+    if (n % i == 0)
+      return false;
+  }
+  return true;
+}
+
+int sqreVal(ll n)
+{
+  // int sqrtN = sqrt(n);
+  int sqrtN = sqrtl(n);
+  // cout << sqrtN << endl;
+  return sqrtN;
+}
+
+void solve()
+{
+  int n;
+  cin >> n;
+  int val =sqreVal(n);
+
+  if(val*val == n){
+    if (isPrime(val))
+      yes;
+    else
+      no;
+  }
+  else
+    no;
+}
+
+int32_t main()
+{
+  fastread();
+
+  int t = 1;
+  cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
