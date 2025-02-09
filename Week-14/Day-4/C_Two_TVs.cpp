a0f50669c02d3ee7b9b277075d53ef93abdcbd4c
Abu Kawsar <121648061+abukawsar45@users.noreply.github.com>
2025-02-09 23:01:22 +0600

    Add files via upload
    week14 day4

---
+++ Week-14/Day-4/C_Two_TVs.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+#define int long long
+#define ll long long
+#define pr pair<int, int>
+#define prl pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define cyes cout << "Yes" << endl
+#define cno cout << "No" << endl
+#define minus cout << -1 << endl
+#define fastread()              \
+  ios_base::sync_with_stdio(0); \
+  cin.tie(0);                   \
+  cout.tie(0);
+
+using namespace std;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+  int n;
+  cin >> n;
+  map<int, int> mp;
+  for (int i = 0; i < n; i++)
+  {
+    int a, b;
+    cin >> a >> b;
+    mp[a]++;
+    mp[b + 1]--;
+  }
+
+  int sum = 0;
+  bool flag = false;
+
+  for (auto [x, y] : mp)
+  {
+    sum += y;
+
+    if (sum > 2)
+    {
+      flag = true;
+      break;
+    }
+  }
+  cout << '\n';
+  if (flag)
+    no;
+  else
+    yes;
+}
+
+int32_t main()
+{
+  fastread();
+  int t = 1;
+  // cin >> t;
+  while (t--)
+  {
+    solve();
+  }
+
+  return 0;
+}
